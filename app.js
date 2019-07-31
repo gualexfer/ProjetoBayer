f9a33ac9148c951879768b2736c85154c50a38c8
bushidorusso <bushido.russo@live.com>
2019-07-30 21:47:09 -0300

    grid na parte da formação

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bayerController = require('./controllers/bayerController');
-
 const app = express();
+const port = 3000;
+
 app.set('view engine', 'ejs');
 
 app.use("/public", express.static(__dirname + "/public"));
 
 bayerController(app);
 
-app.listen(3000, function() {
-    console.log("Ouvindo a porta 3000.");
+app.use((req, res, next) => {
+    if (!req.secure)
+        return res.redirect("submeterCurriculo");
+    next();
+});
+
+app.listen(port, () => {
+    console.log("Server online em http://localhost:" + port);
 });
