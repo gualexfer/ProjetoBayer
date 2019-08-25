48f3bff337b39dcb29211e11f4a1ccb32cee457d
ZangiefWins <fakesupremo2013@hotmail.com>
2019-08-24 21:20:49 -0300

    removidos console.log

--- controllers/bayerController.js
+++ controllers/bayerController.js
@@ -15,8 +15,6 @@ module.exports = function(app) {
 
     app.post('/submeterCurriculo', urlencodedParser, function(req, res) {
         let curriculo = req.body;
-        console.log('-----recebido:-------')
-        console.log(curriculo);
 
         Curriculo(curriculo).save(function(err) {
             if (err) throw err;
